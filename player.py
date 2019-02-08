681e42de2d04ad8a43040f146aa042235c95a0bf
123v3n4n7 <31564068+123v3n4n7@users.noreply.github.com>
2019-02-08 13:11:12 +0300

    Delete player.py

--- player.py
+++
@@ -1,9 +0,0 @@
-import sys
-import
-# from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-#
-# class Player(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-#         self.initUi()
-#     def initUi(self):
